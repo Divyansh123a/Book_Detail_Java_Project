d93b7b90517bff62fc5e0c311932287c5e04bdc5
Divyansh123a <43573674+Divyansh123a@users.noreply.github.com>
2021-06-15 12:19:56 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,35 @@
+
+public class Book {
+
+    // Fill the code here
+    private String bookName;
+    private int bookPrice;
+    private String authorName;
+    public void setBookName(String bookName)
+    {
+        this.bookName = bookName;
+    }
+    public void setBookPrice(int bookPrice)
+    {
+        this.bookPrice = bookPrice;
+    }
+    public void setAuthorName(String authorName)
+    {
+        this.authorName = authorName;
+    }
+    public String getBookName()
+    {
+        return this.bookName;
+    }
+    public int getBookPrice()
+    {
+        return this.bookPrice;
+    }
+    public String getAuthorName()
+    {
+        return this.authorName;
+    }
+    
+
+
+}
